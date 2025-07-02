112330ae983ff0eda513bff8058739c2c9f15de6
Conor Mulvihill <cmulvo@gmail.com>
2025-07-02 21:19:14 +0100

    More robust comparison for illegal cmdrs

--- app.py
+++ app.py
@@ -4,21 +4,36 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 def get_random_card():
+    # Inital API call to scryfall
     api_url = "https://api.scryfall.com/cards/random?q=is%3Acommander"
     response = requests.get(api_url)
     data = response.json()
 
-    # print(data)
+    # Set vars for distunguishing illegal cmdrs
     print("URL:", data["uri"])
     games = data["games"]
-    print("Games:", games)
+    typ = data["type_line"]
+    oracle = data["oracle_text"]
+    set_name = data["set"]
 
+    # Setup error card, mental misstep for illegal cmdrs
+    error_api_url = "https://api.scryfall.com/cards/61e9c6df-1c84-4eab-9076-a4feb6347c10"
+    error_response = requests.get(error_api_url)
+    error_data = error_response.json()
+    error_card = error_data["image_uris"]["normal"]
+
+    # Check card against vars, returning either card or error_card
     if len(games) == 1 and games[0] == "arena":
         print("ARENA ONLY CARD!!!")
-        error_api_url = "https://api.scryfall.com/cards/61e9c6df-1c84-4eab-9076-a4feb6347c10"
-        error_response = requests.get(error_api_url)
-        error_data = response.json()
-        error_card = data["image_uris"]["normal"]
+        return error_card
+    elif len(games) == 1 and games[0] == "mtgo":
+        print("MTGO ONLY CARD!!!")
+        return error_card
+    elif "Creature" not in typ and "can be your commander" not in oracle:
+        print("NOT A LEGAL CMDR CARD!!!")
+        return error_card
+    elif set_name in {"ugl" , "unh" , "ust" , "und" , "unf"}:
+        print("UN-SET!!!")
         return error_card
     else:
         print("NORMAL CARD")
