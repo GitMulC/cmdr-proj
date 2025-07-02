2323f6d307966d3dd33e1297823a72f700a587ec
Conor Mulvihill <cmulvo@gmail.com>
2025-07-02 20:15:34 +0100

    Add arena card logic and debugging statements

--- app.py
+++ app.py
@@ -7,8 +7,23 @@ def get_random_card():
     api_url = "https://api.scryfall.com/cards/random?q=is%3Acommander"
     response = requests.get(api_url)
     data = response.json()
-    card = data["image_uris"]["normal"]
-    return card
+
+    # print(data)
+    print("URL:", data["uri"])
+    games = data["games"]
+    print("Games:", games)
+
+    if len(games) == 1 and games[0] == "arena":
+        print("ARENA ONLY CARD!!!")
+        error_api_url = "https://api.scryfall.com/cards/61e9c6df-1c84-4eab-9076-a4feb6347c10"
+        error_response = requests.get(error_api_url)
+        error_data = response.json()
+        error_card = data["image_uris"]["normal"]
+        return error_card
+    else:
+        print("NORMAL CARD")
+        card = data["image_uris"]["normal"]
+        return card
 
 @app.route('/', methods = ["GET"])
 def cmdr():
