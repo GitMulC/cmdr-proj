89a1defaf43402b351f75d3c7eb5d6e7d0e3dcb1
Conor Mulvihill <cmulvo@gmail.com>
2025-09-01 00:57:50 +0100

    Add default img, brand colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#f5ff6f7',
+          100: '#e0e2e4',
+          200: '#c2c6ca',
+          300: '#a3aab0',
+          400: '#707980',
+          500: '#353b41', // MAIN COLOUR!!!
+          600: '#2f343a',
+          700: '#282c32',
+          800: '#212429',
+          900: '#1a1c21',
+        },
+      },
+    },
   },
   plugins: [],
 }
