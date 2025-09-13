9df17cd6d0559f80116715027d8caf1c7f75e588
Conor Mulvihill <cmulvo@gmail.com>
2025-09-13 22:10:18 +0100

    Prep Render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import requests, random
+import requests, random, os
 from flask import Flask, render_template, jsonify
 from datetime import datetime, date
 from flask_cors import CORS
@@ -194,4 +194,5 @@ def get_bkgr():
                     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
