70d88844fbb6d7bd04318b607a7edd2719263f2c
Conor Mulvihill <cmulvo@gmail.com>
2025-09-14 00:58:52 +0100

    Adjust tailwind configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: ["./templates/**/*.html",
+            "./index.html"
+  ],
   theme: {
     extend: {
       fontFamily: {
