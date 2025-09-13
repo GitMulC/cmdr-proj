9b62ca03d99912a9e1400030e5435e85d37dc932
Conor Mulvihill <cmulvo@gmail.com>
2025-09-13 22:30:39 +0100

    Explicit backend cmds

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, jsonify
 from datetime import datetime, date
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__,
+                    template_folder="../frontend/templates",
+                    static_folder="../frontend/static")
 CORS(app)
 
 def get_random_card():
@@ -195,4 +197,4 @@ def get_bkgr():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
